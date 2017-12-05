916edd87ef87c58919d5d232a1d4b2f76f5a4a01
James Daly <jamesdaly@DBXDs-MacBook-Pro-2.local>
2017-12-05 14:03:38 -0500

    updating test script

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
-console.log(window.location.origin);
+console.log(document.currentScript);
+var span = document.createElement('span'),
+currentSite = "http://www.dbxd.com",
+websiteCredits = "Website Credits",
+websiteHover = "Website by DBXD",
+anchor = document.createElement('anchor'),
+anchorText = document.createTextNode('Website Credits')
+anchor.setAttribute('href', currentSite);
+anchor.setAttribute('target', "_blank");
+anchor.appendChild(anchorText);
+span.appendChild(anchor);
+
+
+function belnord () {
+    var container = document.getElementById('container');
+    container.appendChild(span)
+}
+
+function location (origin) {
+	switch (origin) {
+	case "http://262fifth.dev.dbxd.com/":
+        return true;
+	case "http://localhost:7000":
+        return belnord();
+	default:
+		return null
+	}
+}
+
+location(window.location.origin);
